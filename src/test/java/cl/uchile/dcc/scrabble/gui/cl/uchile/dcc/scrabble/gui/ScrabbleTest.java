62aa61cb89b420c6e14a4c9fa7964b6aeba4956d
unknown <danielngc1@gmail.com>
2021-05-09 02:12:44 -0400

    branchT1

--- src/test/java/cl/uchile/dcc/scrabble/gui/cl/uchile/dcc/scrabble/gui/ScrabbleTest.java
+++ src/test/java/cl/uchile/dcc/scrabble/gui/cl/uchile/dcc/scrabble/gui/ScrabbleTest.java
@@ -1,4 +1,11 @@
+package cl.uchile.dcc.scrabble.gui;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class ScrabbleTest {
-  
+
+    @Test
+    void start() {
+    }
 }
