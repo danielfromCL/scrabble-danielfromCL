3d79a9e07bed1c155b642e829e3d5869f33a5f01
unknown <danielngc1@gmail.com>
2021-05-25 21:21:15 -0400

    Entrega-Parcial-2

--- src/main/java/cl/uchile/dcc/scrabble/gui/INumbers.java
+++ src/main/java/cl/uchile/dcc/scrabble/gui/INumbers.java
@@ -1,2 +1,9 @@
-package cl.uchile.dcc.scrabble.gui;public interface INumbers {
+package cl.uchile.dcc.scrabble.gui;
+
+/**
+ * Interface that implements all the interactions between the subclasses of the Numbers class.
+ */
+public interface INumbers {
+
+    
 }
--- src/main/java/cl/uchile/dcc/scrabble/gui/Numbers.java
+++ src/main/java/cl/uchile/dcc/scrabble/gui/Numbers.java
@@ -5,7 +5,7 @@ package cl.uchile.dcc.scrabble.gui;
  * Abstract class that represents every possible number type in Types.
  * For the first partial submission it has no specific implementations.
  */
-public abstract class Numbers extends Types {
+public abstract class Numbers extends Types implements INumbers{
 
     public Numbers() {
     }
